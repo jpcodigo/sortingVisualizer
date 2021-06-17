9453c778befe7783e51f73a5bea1b549d5960198
Jesse Priest <68927440+jpcodigo@users.noreply.github.com>
2021-06-17 16:42:16 -0500

    finished modding quick sort animation

--- dist/index.fc70f76c.js
+++ dist/index.fc70f76c.js
@@ -727,42 +727,30 @@ function mergeSort(animations) {
   }
 }
 function quickSort(animations) {
-  let i = 0;
-  const arrayBars = document.getElementsByClassName("array-bar");
-  while (animations.length > 0) {
-    if (!Array.isArray(animations[0])) {
-      const pivotIdx = animations.shift();
-      const pivotStyle = arrayBars[pivotIdx].style;
+  for (let i = 0; i < animations.length; i++) {
+    const arrayBars = document.getElementsByClassName("array-bar");
+    const isColorChange = i % 3 !== 2;
+    const [barOneIdx, barTwoIdx] = animations[i];
+    if (isColorChange) {
+      const barOneStyle = arrayBars[barOneIdx].style;
+      const barTwoStyle = arrayBars[barTwoIdx].style;
+      const gradient = i % 3 === 0 ? "linear-gradient(black, lime)" : "linear-gradient(blue, black)";
       setTimeout(() => {
-        for (const bar of arrayBars) {
-          bar.style.backgroundImage = "linear-gradient(blue, black)";
-          bar.style.width = ".4rem";
-        }
-        pivotStyle.backgroundImage = "linear-gradient(white, white)";
-        pivotStyle.width = ".8rem";
+        barOneStyle.backgroundImage = gradient;
+        barTwoStyle.backgroundImage = gradient;
       }, i * animationSpeed);
     } else {
-      const [left, right] = animations.shift();
       setTimeout(() => {
-        const higher = arrayBars[left];
-        const lower = arrayBars[right];
-        higher.style.backgroundImage = "linear-gradient(lime, lime)";
-        lower.style.backgroundImage = "linear-gradient(red, red)";
+        const barOne = arrayBars[barOneIdx];
+        const barTwo = arrayBars[barTwoIdx];
         const temp = document.createElement("div");
-        lower.parentNode.insertBefore(temp, lower);
-        higher.parentNode.insertBefore(lower, higher);
-        temp.parentNode.insertBefore(higher, temp);
+        barOne.parentNode.insertBefore(temp, barOne);
+        barTwo.parentNode.insertBefore(barOne, barTwo);
+        temp.parentNode.insertBefore(barTwo, temp);
         temp.parentNode.removeChild(temp);
       }, i * animationSpeed);
     }
-    i++;
   }
-  setTimeout(() => {
-    for (const bar of arrayBars) {
-      bar.style.backgroundImage = "linear-gradient(blue, black)";
-      bar.style.width = ".4rem";
-    }
-  }, i * animationSpeed);
 }
 function heapSort(animations) {
   for (let i = 0; i < animations.length; i++) {
--- view.js
+++ view.js
@@ -22,9 +22,9 @@ export function mergeSort(animations) {
   for (let i = 0; i < animations.length; i++) {
     const arrayBars = document.getElementsByClassName("array-bar");
     const isColorChange = i % 3 !== 2;
+
     if (isColorChange) {
       const [barOneIdx, barTwoIdx] = animations[i];
-
       const barOneStyle = arrayBars[barOneIdx].style;
       const barTwoStyle = arrayBars[barTwoIdx].style;
       const gradient =
@@ -39,6 +39,7 @@ export function mergeSort(animations) {
       setTimeout(() => {
         const [barOneIdx, newHeight] = animations[i];
         const barOneStyle = arrayBars[barOneIdx].style;
+
         barOneStyle.height = `${newHeight / 10}rem`;
       }, i * animationSpeed);
     }
@@ -46,51 +47,11 @@ export function mergeSort(animations) {
 }
 
 export function quickSort(animations) {
-  let i = 0;
-  const arrayBars = document.getElementsByClassName("array-bar");
-
-  while (animations.length > 0) {
-    if (!Array.isArray(animations[0])) {
-      const pivotIdx = animations.shift();
-      const pivotStyle = arrayBars[pivotIdx].style;
-
-      setTimeout(() => {
-        for (const bar of arrayBars) {
-          bar.style.backgroundImage = "linear-gradient(blue, black)";
-          bar.style.width = ".4rem";
-        }
-        pivotStyle.backgroundImage = "linear-gradient(white, white)";
-        pivotStyle.width = ".8rem";
-      }, i * animationSpeed);
-    } else {
-      const [left, right] = animations.shift();
-      setTimeout(() => {
-        const higher = arrayBars[left];
-        const lower = arrayBars[right];
-        higher.style.backgroundImage = "linear-gradient(lime, lime)";
-        lower.style.backgroundImage = "linear-gradient(red, red)";
-        const temp = document.createElement("div");
-        lower.parentNode.insertBefore(temp, lower);
-        higher.parentNode.insertBefore(lower, higher);
-        temp.parentNode.insertBefore(higher, temp);
-        temp.parentNode.removeChild(temp);
-      }, i * animationSpeed);
-    }
-    i++;
-  }
-  setTimeout(() => {
-    for (const bar of arrayBars) {
-      bar.style.backgroundImage = "linear-gradient(blue, black)";
-      bar.style.width = ".4rem";
-    }
-  }, i * animationSpeed);
-}
-
-export function heapSort(animations) {
   for (let i = 0; i < animations.length; i++) {
     const arrayBars = document.getElementsByClassName("array-bar");
     const isColorChange = i % 3 !== 2;
     const [barOneIdx, barTwoIdx] = animations[i];
+
     if (isColorChange) {
       const barOneStyle = arrayBars[barOneIdx].style;
       const barTwoStyle = arrayBars[barTwoIdx].style;
@@ -107,6 +68,39 @@ export function heapSort(animations) {
         const barOne = arrayBars[barOneIdx];
         const barTwo = arrayBars[barTwoIdx];
         const temp = document.createElement("div");
+
+        barOne.parentNode.insertBefore(temp, barOne);
+        barTwo.parentNode.insertBefore(barOne, barTwo);
+        temp.parentNode.insertBefore(barTwo, temp);
+        temp.parentNode.removeChild(temp);
+      }, i * animationSpeed);
+    }
+  }
+}
+
+export function heapSort(animations) {
+  for (let i = 0; i < animations.length; i++) {
+    const arrayBars = document.getElementsByClassName("array-bar");
+    const isColorChange = i % 3 !== 2;
+    const [barOneIdx, barTwoIdx] = animations[i];
+
+    if (isColorChange) {
+      const barOneStyle = arrayBars[barOneIdx].style;
+      const barTwoStyle = arrayBars[barTwoIdx].style;
+      const gradient =
+        i % 3 === 0
+          ? "linear-gradient(black, lime)"
+          : "linear-gradient(blue, black)";
+      setTimeout(() => {
+        barOneStyle.backgroundImage = gradient;
+        barTwoStyle.backgroundImage = gradient;
+      }, i * animationSpeed);
+    } else {
+      setTimeout(() => {
+        const barOne = arrayBars[barOneIdx];
+        const barTwo = arrayBars[barTwoIdx];
+        const temp = document.createElement("div");
+
         barOne.parentNode.insertBefore(temp, barOne);
         barTwo.parentNode.insertBefore(barOne, barTwo);
         temp.parentNode.insertBefore(barTwo, temp);
